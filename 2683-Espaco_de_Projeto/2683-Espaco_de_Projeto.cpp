c64dce48d0ea8b09e89d412821b0e86a1f05d9ff
Vinicius Alves Beló <vinialvesbelo@outlook.com>
2022-08-13 22:35:36 -0300

    Fourth Commit

--- 2683-Espaco_de_Projeto/2683-Espaco_de_Projeto.cpp
+++ 2683-Espaco_de_Projeto/2683-Espaco_de_Projeto.cpp
@@ -1,3 +1,5 @@
+//Equipe: Piazada, Alunos: Vinicius Alves Beló, Willian Penteado e Felipe Zorzo
+
 #include <iostream>
 #include <queue>
 
@@ -8,7 +10,7 @@ void Make_Set (int *A, int i) {
 }
 
 int MST_Kruskal (priority_queue<pair<int, pair<int, int>>> aresta) {
-    int contador = 0, aux, A[1000] = {};
+    int contador = 0, aux, A[1000];
     for (int i = 0; i < 1000; i++) {
         Make_Set(A, i);
     }
@@ -18,9 +20,9 @@ int MST_Kruskal (priority_queue<pair<int, pair<int, int>>> aresta) {
             contador += aresta.top().first;
             aux = A[aresta.top().second.first];
             A[aresta.top().second.first] = A[aresta.top().second.second];
-            for (int j = 0; j < 1000; j++) {
-                if (A[j] == aux) {
-                    A[j] = aresta.top().second.second;
+            for (int i = 0; i < 1000; i++) {
+                if (A[i] == aux) {
+                    A[i] = A[aresta.top().second.second];
                 }
             }
         }
@@ -29,25 +31,23 @@ int MST_Kruskal (priority_queue<pair<int, pair<int, int>>> aresta) {
     return contador;
 }
 
-int main () {
+main () {
     int n, u, v, w, contador = 0;
     
-    priority_queue<pair<int, pair<int, int>>> caminhoMinimo;
     priority_queue<pair<int, pair<int, int>>> caminhoMaximo;
+    priority_queue<pair<int, pair<int, int>>> caminhoMinimo;
 
     cin >> n;
 
     for (int i = 0; i < n; i++) {
         cin >> u >> v >> w;
-        caminhoMinimo.push(make_pair(w, make_pair(u, v)));
-        caminhoMaximo.push(make_pair(w * -1, make_pair(u, v)));
+        caminhoMaximo.push(make_pair(w, make_pair(u, v)));
+        caminhoMinimo.push(make_pair(w * -1, make_pair(u, v)));
     }
 
-    contador = MST_Kruskal(caminhoMinimo);
+    contador = MST_Kruskal(caminhoMaximo);
     cout << contador << endl;
 
-    contador = MST_Kruskal(caminhoMaximo);
+    contador = MST_Kruskal(caminhoMinimo);
     cout << contador * -1 << endl;
-
-    return 0;
 }
